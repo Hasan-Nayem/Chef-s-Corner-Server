2506b57501b188f8203c22371ddabdd290e3bd2a
Nayem <nayemmh66@gmail.com>
2023-05-03 06:51:01 +0600

    CORS installed

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = 3000;
 const chef = require("./data/chefs.json")
+
+app.use(cors())
 
 app.get('/', (req, res) =>{
     res.send("Server is running");
